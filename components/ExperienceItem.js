a06a4d6ad135ebb026a3df0af842573008335890
Hanna Ton That <hannatonthat@gmail.com>
2025-05-25 23:21:09 -0400

    Experience formatting

--- components/ExperienceItem.js
+++ components/ExperienceItem.js
@@ -2,29 +2,32 @@ import Image from "next/image";
 
 export default function ExperienceItem({ image, company, title, date, location, summary = [] }) {
   return (
-    <div className="">
+    <div className="flex gap-4">
       {image && (
-        <Image
-          src={image}
-          alt=""
-          width="40"
-          height="40"
-          className="object-contain mb-2 rounded-full"
-        />
+        <div className="w-14 h-14 rounded-full bg-zinc-800 flex items-center justify-center mb-2">
+          <Image
+            src={image}
+            alt=""
+            width="40"
+            height="40"
+            className="object-contain rounded-full"
+          />
+        </div>
       )}
+      <div className="mb-8">
+        <h3 className="">{company}</h3>
+        <p className="">{title}</p>
+        <p className="">{date}</p>
+        <p className="">{location}</p>
 
-      <h3 className="">{company}</h3>
-      <p className="">{title}</p>
-      <p className="">{date}</p>
-      <p className="">{location}</p>
-
-      {summary.length > 0 && (
-        <ul className="list-disc list-inside">
-          {summary.map((point, idx) => (
-            <li key={idx}>{point}</li>
-          ))}
-        </ul>
-      )}
+        {summary.length > 0 && (
+          <ul className="list-disc list-inside">
+            {summary.map((point, idx) => (
+              <li key={idx}>{point}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
