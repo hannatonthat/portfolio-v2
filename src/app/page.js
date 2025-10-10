ab86b6d4cf6a8d7ab97f0705dd12b1c4dfafca14
Hanna Ton That <hannatonthat@gmail.com>
2025-10-10 15:51:57 -0400

    Staggered line

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import About from "@components/About";
 import Experience from '@components/Experience';
+import StaggeredLine from '@components/StaggeredLine';
 
 export default function Home() {
   return (
@@ -13,14 +14,16 @@ export default function Home() {
       <section id="experience" className="mt-8">
         <Experience />
       </section>
-
-      <div className="flex mt-8">
-        <a href="/projects">
-          <button className="bg-zinc-200 dark:bg-zinc-800 text-sm px-2 py-1 rounded-md transition-transform duration-200 hover:scale-105 cursor-pointer">
-            View my projects →
-          </button>
-        </a>
-      </div>
+      
+      <StaggeredLine index={8}>
+        <div className="flex mt-8">
+          <a href="/projects">
+            <button className="bg-zinc-200 dark:bg-zinc-800 text-sm px-2 py-1 rounded-md transition-transform duration-200 hover:scale-105 cursor-pointer">
+              View my projects →
+            </button>
+          </a>
+        </div>
+      </StaggeredLine>
     </main>
   );
 }
